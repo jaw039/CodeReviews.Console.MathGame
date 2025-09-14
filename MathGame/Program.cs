db23195e1d2ea91b1d7a7742f34f4b1061534612
jaw039 <jaw039@ucsd.edu>
2025-09-14 11:26:26 -0700

    Add initial menu and requirements for Math game
    Added a requirements comment outlining the Math game features and displayed a menu for operation selection and history viewing. This sets up the basic structure for further development.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+1. You need to create a Math game containing the 4 basic operations
+
+2. The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. 
+Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.
+
+3. Users should be presented with a menu to choose an operation
+
+4. You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
+
+You don't need to record results on a database. Once the program is closed the results will be deleted.
+*/
+
+
+Console.WriteLine("1. Addition \n2. Subtraction \n3. Multiplication \n4. Division \n5. View History \n6. Exit");
